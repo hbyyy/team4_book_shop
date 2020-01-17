a42096dfb0d6b4b8e6398ebfac1c4a157cc9bb30
kimdooh2019 <dopa213@naver.com>
2020-01-17 14:31:32 +0900

    view 추가

--- app/books/views/detail.py
+++ app/books/views/detail.py
@@ -4,11 +4,9 @@ from books.models import Book
 
 
 def book_detail_view(request, book_pk):
-    selected_book = Book.objects.filter(pk=book_pk)
-
-
+    selected_book = Book.objects.filter(pk=book_pk).values()[0]
     context = {
-
+        'selected_book' : selected_book,
     }
 
     return render(request, 'book_detail_info.html', context)
