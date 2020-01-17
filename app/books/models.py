d0e2d3efcd0580d7ba23372eb39414879b972aad
hbyyy <qjaduddl94@gmail.com>
2020-01-17 11:11:27 +0900

    Book의 image 업로드 경로 설정

--- app/books/models.py
+++ app/books/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 
 # Create your models here.
+from config import settings
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
     book_intro = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='books/image')
 
     def __str__(self):
         return self.title
