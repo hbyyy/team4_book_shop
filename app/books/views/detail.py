84e4ff003e9ea166078d552a95a119971d361fe9
kimdooh2019 <dopa213@naver.com>
2020-01-17 13:01:55 +0900

    상세정보 파일 업데잍트

--- app/books/views/detail.py
+++ app/books/views/detail.py
@@ -1,3 +1,14 @@
-def book_detail_view(request):
-    pass
+from django.shortcuts import render
 
+from books.models import Book
+
+
+def book_detail_view(request, book_pk):
+    selected_book = Book.objects.filter(pk=book_pk)
+
+
+    context = {
+
+    }
+
+    return render(request, 'book_detail_info.html', context)
