fc2aafaf7b4964b40f5d41e2768d5fa442b00fef
eunbiviakwon <eunbiviakwon@gmail.com>
2020-01-17 15:28:36 +0900

    request 추가

--- app/books/views/request.py
+++ app/books/views/request.py
@@ -1,3 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .. import crawler
+from ..crawler import research_page_crawler
+
 
 def book_request_view(request):
-    pass
+    if request.method == 'POST':
+        value = request.POST['q']
+        book_list = research_page_crawler(value)
+        print('----------------------------------------')
+        print(book_list)
+        print('----------------------------------------')
+        context = {
+            'book_list': book_list
+        }
+        return render(request, 'request.html', context)
+
+    elif request.method == 'GET':
+        value = request.GET.get('keyword', 'java')
+        book_list = research_page_crawler(value)
+        print('----------------------------------------')
+        print(book_list)
+        print('----------------------------------------')
+        context = {
+            'book_list': book_list
+        }
+        return render(request, 'request.html', context)
+
+
+
