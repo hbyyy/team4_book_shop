32f1f51c8cb504945d0ab79b3fc414d7aa57a4a2
kimdooh2019 <dopa213@naver.com>
2020-01-17 13:01:55 +0900

    상세정보 파일 업데잍트

--- app/books/urls.py
+++ app/books/urls.py
@@ -3,7 +3,7 @@ from .views import detail, create, request
 
 app_name = 'books'
 urlpatterns = [
-    path('detail/', detail.book_detail_view, name='detail'),
+    path('detail/<int:book_pk>', detail.book_detail_view, name='detail'),
     path('book-create/',create.book_create_view, name='book-create' ),
     path('book-request/', request.book_request_view, name='book-request'),
 
