ea3494fb6ae1a122e69028710228d9325a080b29
kimdooh2019 <dopa213@naver.com>
2020-02-01 10:22:52 +0900

    sqlite -> AWS_RDS_postgresql DB 교체함 makemigrations && migrate 함

--- app/config/settings.py
+++ app/config/settings.py
@@ -94,8 +94,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+		# 데이터베이스 이름
+        'NAME': 'instagram',
+		# RDS username && password
+		'USER': 'luka',
+		'PASSWORD':'asdfghjkl',
+		# RDS endpoint
+		'HOST':'book-shop.cqypwjxqtvck.ap-northeast-2.rds.amazonaws.com',
+		# default port number
+		'PORT':'5432',
     }
 }
 
