06a2f7d1e7f5c7cce99a81207c12b7d47b19f29c
hbyyy <qjaduddl94@gmail.com>
2020-02-09 20:52:17 +0900

    feat: 대출 반납 늦게 할 시 늦은 만큼 대출하지 못하도록 구현

--- app/books/views/main.py
+++ app/books/views/main.py
@@ -1,14 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.utils import timezone
 
 from books.models import Book
 
 
-def book_list_view(request):
+def main_page_view(request):
     if request.user.is_authenticated:
         books = Book.objects.order_by('title')
+        user =request.user
+        if user.ban_date < timezone.now():
+            user.ban_date = None
+            user.save()
+
         context = {
-            'books': books
+            'books': books,
+            'current_date': timezone.now(),
+            'ban_date': user.ban_date
         }
         return render(request, 'books/main.html', context)
     else:
--- app/rental/models.py
+++ app/rental/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 from django.utils import timezone
 
+from members.models import User
+
 
 class Rental(models.Model):
     book = models.OneToOneField('books.Book', on_delete=models.CASCADE)
@@ -15,6 +17,14 @@ class Rental(models.Model):
     def save(self, *args, **kwargs):
         self.end_date = self.start_date + timedelta(days=1)
         super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        now = timezone.now()
+        if self.end_date < now:
+            rate_timedelta = now - self.end_date
+            self.user.ban_date = now + rate_timedelta
+            self.user.save()
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         return f'rental : {self.book}, {self.user}'
