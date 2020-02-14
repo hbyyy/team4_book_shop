1976e88c77b945cf558f12f5ab90144130237ffe
hbyyy <qjaduddl94@gmail.com>
2020-02-14 15:57:18 +0900

    feat: 책 예약, 예약 취소 기능 추가 - 책 예약은 1명만 가능

--- app/reservation/views.py
+++ app/reservation/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+
+from books.models import Book
+from reservation.models import Reservation
 
 
-def reservation_view(request):
-    pass
+def reservation_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    Reservation.objects.create(book=book, user=request.user)
+    return redirect('books:book-list')
+
+
+def reservation_cancel_view(request, pk):
+    reservation = get_object_or_404(Reservation, pk=pk)
+    reservation.delete()
+    return redirect('books:book-list')
